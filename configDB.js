85774a88e5d6b1fcceb349f420f550aa8c30cbbd
Ratik Vig <vig.ratik@tftus.com>
2021-06-16 23:45:50 +0530

    database connection and router setup

---
+++ configDB.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    } catch (error) {
+        console.log('error')
+    }
+}
+
+module.exports = connectToDB
+
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const connectToDB = require('./configDB')
+
+const users = require('./routes/api/users')
+
+require('dotenv').config()
 
 const app = express()
+const PORT = process.env.PORT || 3000
+connectToDB()
 
-const PORT = process.env.PORT || 5000
+app.use('/api/v1/users', users)
 
 app.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`)
